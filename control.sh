5e9e06187cf00067ee62c44c7ecaf3cc046a102a
Joachim <47549862+Schokokex@users.noreply.github.com>
2020-11-25 22:21:41 +0100

    (cd in brackets is only local), outside doesnt know

--- control.sh
+++ control.sh
@@ -4,5 +4,5 @@ echo APP_NAME=$APP_NAME
 echo GIT_URL=$GIT_URL
 echo PROJECT_PATH=$PROJECT_PATH
 
-cd /root/ && (git clone $GIT_URL $APP_NAME 2>/dev/null && cd $APP_NAME) || ( cd $APP_NAME && git pull) && cd $PROJECT_PATH && npm install && (npm test || (echo proceed? && read)) && npm run build
+cd /root/ && git clone $GIT_URL $APP_NAME 2>/dev/null || (cd $APP_NAME && git pull) && cd $APP_NAME && cd $PROJECT_PATH && npm install && (npm test || (echo proceed? && read)) && npm run build
 npm start
