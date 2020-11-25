7ea3189b57d45ca19272a94ac2a4cd13cf9b8e50
Joachim <47549862+Schokokex@users.noreply.github.com>
2020-11-25 21:49:12 +0100

    Update control.sh

--- control.sh
+++ control.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo APP_NAME=$APP_NAME
-echo GIT_REPO=$GIT_REPO
+echo GIT_URL=$GIT_URL
 
-git clone $GIT_REPO $APP_NAME 2>/dev/null || ( cd $APP_NAME && git pull) && npm install && (npm test || (echo proceed? && read)) && npm run build
+git clone $GIT_URL $APP_NAME 2>/dev/null || ( cd $APP_NAME && git pull) && npm install && (npm test || (echo proceed? && read)) && npm run build
 npm start
